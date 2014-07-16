5aadc1b5b19918914d842c3a16c8dd69654d4b67
clmc <clmc@icloud.com>
2014-07-16 15:13:39 -0400

    broken value recognition loop

--- src/main.c
+++ src/main.c
@@ -8,6 +8,9 @@ int playerState[1][8];
 
 
 int main(){
+
+    int sensitiveCheck, playerSensitive, x;
+
 	playerState[0][0];
 	playerState[0][1];
 	playerState[0][2];
@@ -17,14 +20,22 @@ int main(){
 	playerState[0][6];
 	playerState[0][7];
 
-
 	printf("Your health begins at %d\n", HEALTH);
 	printf("Your magic begins at %d\n", MAGIC);
 
 	HEALTH = 10;
 
 	printf("From there your health defaults to %d\n", HEALTH);
-	printf("But your magic remains at %d most of the time.\n", MAGIC);
+	printf("But your magic remains at %d most of the time.\n\nWell, mostly. Are you magic sensitive? \n", MAGIC);
+
+	if (( sensitiveCheck = scanf("%d", &x)) == 0) {
+	    printf("Nope, %d !\n");
+	    playerSensitive = 0; }
+	if (( sensitiveCheck = scanf("%d", &x)) == 1) {
+	    printf("Yup, %d !\n");
+	    playerSensitive = 1; }
+	else {
+	    printf("Please enter 0 if you are not magic sensitive, or 1 if you are.\n");}
 
 return 0;
 }
