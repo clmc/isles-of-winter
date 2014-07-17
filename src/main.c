dcc638d393fcc98119c671eaa72180c63dee6194
clmc <clmc@icloud.com>
2014-07-17 16:57:06 -0400

    fixed mistake

--- src/main.c
+++ src/main.c
@@ -12,15 +12,6 @@ int main(){
     int inputMagic;
     char playerSensitive;
 
-	playerState[0][0];
-	playerState[0][1];
-	playerState[0][2];
-	playerState[0][3];
-	playerState[0][4];
-	playerState[0][5];
-	playerState[0][6];
-	playerState[0][7];
-
 	printf("Your health begins at %d\n", HEALTH);
 	printf("Your magic begins at %d\n", MAGIC);
 
