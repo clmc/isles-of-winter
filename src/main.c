8faa856315bec89246fc21a12ec788886fc07f33
clmc <clmc@icloud.com>
2014-07-18 16:00:56 -0400

    attempting character input

--- src/main.c
+++ src/main.c
@@ -10,24 +10,27 @@ int playerState[1][8];
 int main(){
 
     int i, playerSensitive, inputMagic;
+    int c = fgetc(stdin);
+    
+    if (c != 'y' || c != 'n')
+	fprintf(stderr, "Nice try.\n");
 
-	printf("Your health begins at %d\n", HEALTH);
-	printf("Your magic begins at %d\n", MAGIC);
+//	printf("Your health begins at %d\n", HEALTH);
+//	printf("Your magic begins at %d\n", MAGIC);
+//
+//	HEALTH = 10; /* Initialize health */
+//
+//	printf("From there your health defaults to %d\n", HEALTH);
+//	printf("But your magic remains at %d most of the time.\n\nWell, mostly. Are you magic sensitive? ", MAGIC);
 
-	HEALTH = 10; /* Initialize health */
+//	scanf("%d", &playerSensitive); /* Take input from player, to determine if they like mages */
 
-	printf("From there your health defaults to %d\n", HEALTH);
-	printf("But your magic remains at %d most of the time.\n\nWell, mostly. Are you magic sensitive? ", MAGIC);
-
-	scanf("%d", &playerSensitive); /* Take input from player, to determine if they like mages */
-
-	"ny"[i]; where i == 0 or i == 1;
-	if (playerSensitive == "n") {
-	    printf("You are not sensitive.\n");}
-	else if (playerSensitive == "y") {
-	    printf("You are magic sensitive!\n");}
-	else {
-	    printf("Please enter 0 if you are not magic sensitive, or 1 if you are.\n");}
+//	if (playerSensitive == 0) {
+//	    printf("You are not sensitive.\n");}
+//	else if (playerSensitive == 1) {
+//	    printf("You are magic sensitive!\n");}
+//	else {
+//	    printf("Please enter 0 if you are not magic sensitive, or 1 if you are.\n");}
 
 return 0;
 }
